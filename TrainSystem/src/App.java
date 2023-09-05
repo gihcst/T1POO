6a8d00beebe96bef5c9988b57228822a298ac1b9
Amanda <work.amannda@gmail.com>
2023-09-05 01:40:55 -0300

    Menu

--- TrainSystem/src/App.java
+++ TrainSystem/src/App.java
@@ -1,5 +1,26 @@
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        scanner scanner = new scanner(System.in);
+        
+        System.out.println("Bem vindo ao aplicativo.");
+
+            do{
+                System.out.println("         Menu         ");
+                System.out.println("   1. Criar trem      ");
+                System.out.println("   2. Editar trem     ");
+                System.out.println("   3. Listar trens    ");
+                System.out.println("   4. Desfazer trem   ");
+                System.out.println("   5. Sair do programa");
+                System.out.println("\n");
+                System.out.print(" Digite o número da opção desejada: ");
+                escolhaMenu = in.nextInt();
+                    if(escolhaMenu < 1 || escolhaMenu > 5){
+                        System.out.println("Opção inválida. Digite novamente.");
+            }while(escolhaMenu > 0 || escolhaMenu < 5);
+            switch(escolhaMenu) {
+                case 1:
+                System.out.println("Você escolheu a opção 1.");
+                break;
     }
 }
---
+++ TrainSystem/src/Patio.java
@@ -0,0 +1,5 @@
+public class Patio {
+    private ArrayList<Trem> trens;
+    GaragemLocomotivas gl;
+    GaragemVagoes gv;
+}
